738309913846e1b1880d9a56524255a0afa45956
alexgim <alexgim961101@gmail.com>
2023-05-24 13:44:48 +0900

    main 메서드로 실행 테스트

---
+++ src/main/java/com/example/mutsa5thtobyspring3/dao/UserDao.java
@@ -0,0 +1,62 @@
+package com.example.mutsa5thtobyspring3.dao;
+
+import com.example.mutsa5thtobyspring3.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/like_lion?serverTimezone=Asia/Seoul", "root", "Rlawlgus12#");
+
+        PreparedStatement ps = c.prepareStatement("insert into User(id, name, password) values(?, ?, ?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/like_lion?serverTimezone=Asia/Seoul", "root", "Rlawlgus12#");
+
+        PreparedStatement ps = c.prepareStatement("select * from User where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("2");
+        user.setName("alex");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user.getName());
+        System.out.println(user.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
---
+++ src/main/java/com/example/mutsa5thtobyspring3/domain/User.java
@@ -0,0 +1,11 @@
+package com.example.mutsa5thtobyspring3.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class User {
+    String id;
+    String name;
+    String password;
+}
